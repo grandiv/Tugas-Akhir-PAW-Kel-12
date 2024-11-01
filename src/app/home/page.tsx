487ecad7b0545a5d8af350aaf3f20f8e262a1361
ranisedangbekerja <raninirmalaprakoso@mail.ugm.ac.id>
2024-11-01 21:13:24 +0700

    link page to homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,7 @@ import Layout from '../layout';
 const HomePage: React.FC = () => {
     return (
         <Layout>
-            <h1 className="text-center mt-10 text-2xl">Ini adalah Home Page</h1>
+            <h1 className="text-center mt-10 text-2xl">Ini adalah homie dummy sweety</h1>
         </Layout>
     );
 };
