4c9ba3477cffd368442813659457ae0be27b7deb
Muhammad Farrel Akbar <142593668+MFarrelAkbar1@users.noreply.github.com>
2024-11-05 13:28:45 +0700

    updated sayurpage
    finished making sayurpage template

--- src/app/sayur/page.tsx
+++ src/app/sayur/page.tsx
@@ -1,12 +1,6 @@
-import React from 'react';
-import Layout from '../layout';
+import React from "react";
+import Seafood from "@/components/Sayur";
 
-const SayurPage: React.FC = () => {
-    return (
-        <Layout>
-            <h1 className="text-center mt-10 text-2xl">Ini adalah Sayur Page</h1>
-        </Layout>
-    );
-};
-
-export default SayurPage;
+export default function SayurPage() {
+  return <Seafood />;
+}
