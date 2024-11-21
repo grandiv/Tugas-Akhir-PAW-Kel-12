88c000df65f560eb98feb457ce2bf1b0f0f72af2
ranisedangbekerja <raninirmalaprakoso@mail.ugm.ac.id>
2024-11-21 19:14:44 +0700

    update product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,4 @@
+// src/app/components/ProductCard.tsx
 import React, { useState } from "react";
 import { useCart } from "@/context/CartContext";
 import {
@@ -17,7 +18,6 @@ interface ProductCardProps {
   desc: string;
   stock: number;
   price: number;
-  netto: string;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({
@@ -26,12 +26,11 @@ const ProductCard: React.FC<ProductCardProps> = ({
   desc,
   stock,
   price,
-  netto,
 }) => {
   const { data: session } = useSession();
   const router = useRouter();
   const { addItemToCart } = useCart();
-  const [showNotification, setShowNotification] = useState(false);
+  const [showNotification, setShowNotification] = useState(false); // State untuk notifikasi
 
   const handleAddToCart = () => {
     if (!session) {
@@ -39,8 +38,9 @@ const ProductCard: React.FC<ProductCardProps> = ({
       return;
     }
     addItemToCart({ imageUrl, name, price, quantity: 1, isChecked: true });
-    setShowNotification(true);
+    setShowNotification(true); // Tampilkan notifikasi
 
+    // Sembunyikan notifikasi setelah 2 detik
     setTimeout(() => {
       setShowNotification(false);
     }, 2000);
@@ -59,14 +59,13 @@ const ProductCard: React.FC<ProductCardProps> = ({
         <CardContent className="pb-2">
           <CardTitle>{name}</CardTitle>
           <CardDescription>{desc}</CardDescription>
-          <div className="flex flex-col items-left mt-2">
-            <CardDescription className="font-bold text-[18px]">
+          <div className="flex flex-row justify-between pt-2">
+            <CardDescription className="font-bold text-[24px]">
               Rp{price}
             </CardDescription>
-            <div className="flex flex-col items-center space-y-1 text-gray-500">
-              <CardDescription>Stock: {stock}</CardDescription>
-              <CardDescription>Netto: {netto}</CardDescription>
-            </div>
+            <CardDescription className="my-auto">
+              Stock: {stock}
+            </CardDescription>
           </div>
         </CardContent>
         <CardFooter>
